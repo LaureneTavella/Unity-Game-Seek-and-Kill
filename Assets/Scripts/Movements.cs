6d9dc9272d3ad1d9342f5bcec282ec18276d2d9d
LaureneTavella <44021846+LaureneTavella@users.noreply.github.com>
2019-12-08 12:16:17 -0500

    added airSpeedMultiplier feature

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class Movements : MonoBehaviour
 {
     public float speed;
-    private Rigidbody rb;
+    public float jumpForce;
+    public float airSpeedMultiplier;
 
+    private Rigidbody rb;
     private Vector3 newPos;
+    private bool isGrounded = true;
+    private float distanceToGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+
+        distanceToGround = this.GetComponent<Collider>().bounds.extents.y;
     }
 
     // Update is called once per frame
@@ -22,12 +28,25 @@ public class Movements : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         newPos = new Vector3(horizontal, 0, vertical);
 
+        UpdateGrounded();
+
         // TODO : smooth
     }
 
     private void FixedUpdate()
     {
-        newPos = newPos.normalized * speed * Time.fixedDeltaTime; // so we don't go faster in diagonal !
-        rb.MovePosition(transform.position + newPos);        
+        float realSpeed = isGrounded ? speed : speed * airSpeedMultiplier;
+
+        newPos = newPos.normalized * realSpeed * Time.fixedDeltaTime; // so we don't go faster in diagonal !
+        rb.MovePosition(transform.position + newPos);
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+               rb.AddForce(transform.up * jumpForce);
+    }
+
+    private void UpdateGrounded()
+    {
+        isGrounded = Physics.Raycast(transform.position, -Vector3.up, distanceToGround + 0.1f);
     }
 }
